c6b4fb5c82695c9bc060fe797dd10ccc3dac3831
codeamac01 <codeamac01@gmail.com>
2017-06-02 18:03:03 -0500

    agregando primer archivo

---
+++ prueba.rb
@@ -0,0 +1,15 @@
+nombre = "geovanni"
+p nombre.upcase
+
+#.upcase, que pone todos los caracteres en mayúsculas
+#.downcase, que pone todos los caracteres en minúsculas
+#.length, que nos dice el número de caracteres de un string, incluyendo los espacios en blanco.
+=begin
+.chr muestra el primer carter de una cadena
+
+reverse, que invierte los caracteres de un string
+swapcase, pone los caracteres mayúsculas en minúsculas y los minúsculas en mayúsculas
+capitalize, pone el primer caracter del string en mayúsculas, y los demás en minúsculas
+slice, da una parte de un string
+
+=end
