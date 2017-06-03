7da4d62bfd6b42e7bacdab91015216d4a27c588c
codeamac01 <codeamac01@gmail.com>
2017-06-02 19:32:56 -0500

    documentacion

--- prueba.rb
+++ prueba.rb
@@ -1,11 +1,19 @@
-nombre = "geovanni"
+nombre = "Geovanni"
 p nombre.upcase
+p nombre.start_with? ("G")
+p nombre.downcase
+p nombre.length
+p nombre.end_with? ("i")
+
+a = "geovanni"
+p a[-1]
+p 
 
 #.upcase, que pone todos los caracteres en mayúsculas
 #.downcase, que pone todos los caracteres en minúsculas
 #.length, que nos dice el número de caracteres de un string, incluyendo los espacios en blanco.
 =begin
-.chr muestra el primer carter de una cadena
+.chr muestra el primer caracter de una cadena
 
 reverse, que invierte los caracteres de un string
 swapcase, pone los caracteres mayúsculas en minúsculas y los minúsculas en mayúsculas
